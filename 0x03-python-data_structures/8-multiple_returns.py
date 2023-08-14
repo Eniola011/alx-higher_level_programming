366440de58f25664fe3e9c71ce82965db4137680
Eniola011 <yettyagbalu@gmail.com>
2023-08-13 22:51:25 -0700

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    my_sentence = len(sentence)
+
+    if my_sentence == 0:
+        my_tuple = (my_sentence, None)
+    else:
+        my_tuple = (my_sentence, sentence[0])
+    return my_tuple
