63acd51c5593b3893795cc3b7fe688148f00689d
Eniola011 <yettyagbalu@gmail.com>
2023-08-28 17:41:50 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-	sumup = 0
-	for elem in range(x):
-		try:
-			print(my_list[elem], end="")
-			sumup += 1
-		except IndexError:
-			break
-	print()
-	return sumup
+    sumup = 0
+    for elem in range(x):
+        try:
+            print(my_list[elem], end="")
+            sumup += 1
+        except IndexError:
+            break
+    print()
+    return sumup
