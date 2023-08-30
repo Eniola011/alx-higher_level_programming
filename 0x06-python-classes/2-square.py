e56e26431a5efc8be35f6e7e6f408baa70780aac
Eniola011 <yettyagbalu@gmail.com>
2023-08-29 18:39:14 -0700

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A square class: defines a square by private instance attribute"""
+
+
+class Square:
+    """size is a private object attribute"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
