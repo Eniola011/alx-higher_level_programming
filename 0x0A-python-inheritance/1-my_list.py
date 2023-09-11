107d4060a6a9846843f92f8050d4b869ca218ad5
Eniola011 <yettyagbalu@gmail.com>
2023-09-11 15:38:35 -0700

    mylist function

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+
+MyList Module
+
+"""
+
+
+class Mylist(list):
+    """ A class that inherits from list
+    Args:
+        list: list
+    """
+
+    def print_sorted(self):
+        """ Public Instance Method:
+            Prints the list, but sorted (ascending sort)
+        """
+
+        sorted_list = self.copy()
+        sorted_list.sort()
+        print(sorted_list)
