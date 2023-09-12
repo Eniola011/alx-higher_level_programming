dba87a1f45574df49f5ab3d90f1acc9a001e0933
Eniola011 <yettyagbalu@gmail.com>
2023-09-11 17:59:50 -0700

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+
+Square Module
+
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Inherits from Rectangle
+    """
+
+    def __init__(self, size):
+        """ Initialize private instance """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        return str("[Square] {}/{}".format(self.__size, self.__size))
+
+    def area(self):
+        """ Area of a Square """
+        return self.__size ** 2
