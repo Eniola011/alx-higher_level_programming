57a691db8b6c78bf75f9cb86525538aa92e16be2
Eniola011 <yettyagbalu@gmail.com>
2023-09-17 16:19:59 -0700

    13-main.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -44,3 +44,12 @@ class Square(Rectangle):
                     setattr(self, 'height', value)
                 else:
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ Returns the dictionary representation of a 'Square' """
+        return {
+            'id': self.id,
+            'size': self.width,
+            'x': self.x,
+            'y': self.y
+        }
