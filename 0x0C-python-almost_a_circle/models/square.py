c305f8583311c6a3e53fb459b9e9a4f5e4e39181
Eniola011 <yettyagbalu@gmail.com>
2023-09-17 15:49:52 -0700

    square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+
+Square.py
+
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Inherits from Rectangle class """
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Initializes instances """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
