13cbaee753030025236eed1c6d00936eeede5ece
Eniola011 <yettyagbalu@gmail.com>
2023-11-13 18:15:55 -0800

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,6 +3,6 @@ function factorial (n) {
   if ((Number.isNaN(n)) || (n === 1)) {
     return 1;
   }
-  return n * factorial(n -1);
+  return n * factorial(n - 1);
 }
 console.log(factorial(parseInt(process.argv[2])));
