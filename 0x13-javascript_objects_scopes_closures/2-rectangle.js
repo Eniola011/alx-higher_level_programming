b76e4f734364017b8e75c29794d7e133bd12e900
Eniola011 <yettyagbalu@gmail.com>
2023-11-14 13:37:35 -0800

    2-rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// The constructor must take 2 arguments w and h
+// If w or h is equal to 0 or not a positive integer, create an empty object
 class Rectangle {
   constructor (w, h) {
     if ((w > 0) && (h > 0)) {
