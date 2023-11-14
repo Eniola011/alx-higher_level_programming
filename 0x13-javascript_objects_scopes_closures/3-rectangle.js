9c5d1f4e80a4452e7be268baa33dcf4f96952ae8
Eniola011 <yettyagbalu@gmail.com>
2023-11-14 13:48:36 -0800

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,10 +1,16 @@
 #!/usr/bin/node
-// The constructor must take 2 arguments w and h
+// Create an instance method called print() that prints the rectangle using the character X
 class Rectangle {
   constructor (w, h) {
     if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
 }
