4d6861e3e4c8581009ee82bbdda3043329b77147
Eniola011 <yettyagbalu@gmail.com>
2023-11-14 13:26:54 -0800

    1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,13 +1,6 @@
 #!/usr/bin/node
-/*
- You must use the class notation for defining your class
- The constructor must take 2 arguments w and h
- Initialize the instance attribute width with the value of w
- Initialize the instance attribute height with the value of h
-*/
+// The constructor must take 2 arguments w and h
 class Rectangle {
-  width;
-  height;
   constructor (w, h) {
     this.width = w;
     this.height = h;
