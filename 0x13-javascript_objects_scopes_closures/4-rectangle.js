badc7ff09026e1031b1666c59465e7ddd8f26da8
Eniola011 <yettyagbalu@gmail.com>
2023-11-14 14:08:23 -0800

    4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-// Create an instance method called print() that prints the rectangle using the character X
+// Create an instance method called rotate() that exchanges the width and the height of the rectangle
+// Create an instance method called double() that multiples the width and the height of the rectangle by 2
 class Rectangle {
   constructor (w, h) {
     if ((w > 0) && (h > 0)) {
@@ -9,9 +10,23 @@ class Rectangle {
   }
 
   print () {
+    // prints the rectangle using the character X
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
+  }
+
+  rotate () {
+    // exchange values
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    // multiplies rectangle by 2
+    this.width *= 2;
+    this.heigth *= 2;
   }
 }
 
