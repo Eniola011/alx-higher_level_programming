0976a06b0d13be1c1c1755ce2b1665b58e8374cb
Eniola011 <yettyagbalu@gmail.com>
2023-11-18 17:50:22 -0800

    model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+
+State Class
+
+"""
+
+
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class State(Base):
+    """Inherits from base"""
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
