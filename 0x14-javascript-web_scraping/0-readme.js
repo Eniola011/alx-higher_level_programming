a505f47cb10ff9f7ecd17287ee0c3f0357591a99
Eniola011 <yettyagbalu@gmail.com>
2024-01-05 15:22:26 -0800

    adjusted code

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
+// Write a script that reads and prints the content of a file.
+const fs = require('fs');
 
-; // Write a script that reads and prints the content of a file.
-
-const fs = require('fs')
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
-  if (data == undefined) {
+  if (data === undefined) {
     console.log(err);
   } else {
       console.log(data);
