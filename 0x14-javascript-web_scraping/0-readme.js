d63e024d9b447461928ba253fe702f7f3632c18b
Eniola011 <yettyagbalu@gmail.com>
2024-01-05 15:13:18 -0800

    reads and prints the content of a file.

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Write a script that reads and prints the content of a file.
+const fs = require('fs')
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (data == undefined) {
+    console.log(err);
+  } else {
+      console.log(data);
+    }
+});
