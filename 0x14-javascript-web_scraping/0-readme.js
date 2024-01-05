3b4703b28c4113b4acb87c7d0c2d63183792e277
Eniola011 <yettyagbalu@gmail.com>
2024-01-05 15:20:22 -0800

    added semicolon to comment

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-// Write a script that reads and prints the content of a file.
+
+; // Write a script that reads and prints the content of a file.
+
 const fs = require('fs')
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (data == undefined) {
