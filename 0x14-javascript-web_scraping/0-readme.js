5dbdfd33c37c088d328edafbf08ff3d31479fb1e
Eniola011 <yettyagbalu@gmail.com>
2024-01-05 16:03:12 -0800

    reads and prints the content of a file.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,6 +6,6 @@ fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (data === undefined) {
     console.log(err);
   } else {
-      console.log(data);
-    }
+    console.log(data);
+  }
 });
