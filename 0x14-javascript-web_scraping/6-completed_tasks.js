650215cdf6728d907946b3672c6d086fbfb7f458
Eniola011 <yettyagbalu@gmail.com>
2024-01-05 17:56:03 -0800

    computes the number of tasks completed by user id.

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Write a script that computes the number of tasks completed by user id.
+const request = require('request');
+
+request(process.argv[2], (error, response, body) => {
+  if (error) throw new Error(error);
+  const tasks = JSON.parse(body);
+  const users = {};
+  tasks.forEach(task => {
+    if (task.completed) {
+      if (!users[task.userId.toString()]) {
+        users[task.userId.toString()] = 0;
+      }
+      users[task.userId.toString()] += 1;
+    }
+  });
+  console.log(users);
+});
